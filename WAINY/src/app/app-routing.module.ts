d0aaed232925429fbdd9def7126d4592fa7893e4
Luca Ceccato <luca.ceccato@teorema.net>
2019-06-07 17:34:20 +0200

    Creata struttura base applicazione con slash screen e chat messaggi

---
+++ WAINY/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {Routes,RouterModule} from '@angular/router'
+import { from } from 'rxjs';
+import {SplashComponent}from "./Container/splash/splash.component";
+import {ChatbotComponent} from "./Container/chatbot/chatbot.component";
+
+const router :Routes=[
+  {
+    path:'',
+    component:SplashComponent
+    },
+    {
+      path:'splash',
+      component:ChatbotComponent
+      }
+];
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(router),
+  ],
+  exports:[RouterModule]
+})
+export class AppRoutingModule { }
---
+++ WAINY/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import {SplashComponent } from './Container/splash/splash.component';
+import { ChatbotComponent } from './Container/chatbot/chatbot.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SplashComponent,
+    ChatbotComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
